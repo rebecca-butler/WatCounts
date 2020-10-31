e77af6493e1cadaba137c122043d1567f9c4d4f3
rebecca-butler <rebutler@uwaterloo.ca>
2020-10-31 15:46:18 -0400

    Updated frame recording and added initial counter

---
+++ src/counter.py
@@ -0,0 +1,47 @@
+from __future__ import print_function
+from imutils.object_detection import non_max_suppression
+from imutils import paths
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+# get frames from video
+exec(open('record_video.py').read())
+
+# initialize the HOG descriptor/person detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+# loop over the image paths to detect people
+for imagePath in paths.list_images('/home/pi/Projects/Videos'):
+    # load image
+	image = cv2.imread(imagePath)
+	image = imutils.resize(image, width=min(400, image.shape[1]))
+	orig = image.copy()
+
+	# detect people in the image
+	(rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),
+		padding=(8, 8), scale=1.05)
+
+	# draw initial bounding boxes
+	for (x, y, w, h) in rects:
+		cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+	# apply non-maxima suppression to bounding boxes (combines overlapping boxes into one)
+	rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+	pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+
+	# draw final bounding boxes
+	for (xA, yA, xB, yB) in pick:
+		cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
+
+	# show info
+	filename = imagePath[imagePath.rfind("/") + 1:]
+	print("[INFO] {}: {} original boxes, {} after suppression".format(
+		filename, len(rects), len(pick)))
+
+	# show the output images
+	cv2.imshow("Before NMS", orig)
+	cv2.imshow("After NMS", image)
+	cv2.waitKey(0)
--- src/record_video.py
+++ src/record_video.py
@@ -1,11 +1,30 @@
 import picamera
-from datetime import date
-from time import sleep
+import cv2
+import time
+import datetime
+from picamera.array import PiRGBArray
 
+# initialize camera settings
 camera = picamera.PiCamera()
-camera.start_preview()
-today = date.today()
-camera.start_recording('/home/pi/Projects/Videos/{}.h264'.format(today))
-sleep(5)
-camera.stop_recording()
-camera.stop_preview()
+camera.framerate = 32
+camera.resolution = (640, 480)
+rawCapture = PiRGBArray(camera, size=(640, 480))
+time.sleep(0.1)
+now = date.now()
+
+# capture frames from camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # show frame
+	image = frame.array
+	cv2.imshow("Frame", image)
+	key = cv2.waitKey(1) & 0xFF
+
+    # save frame on disk
+    cv2.imwrite('/home/pi/Projects/Videos/{}.jpg'.format(datetime.datetime.now()), image)
+
+    # clear stream for next frame
+	rawCapture.truncate(0)
+
+	# break if q is pressed
+	if key == ord("q"):
+		break
