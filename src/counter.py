aa4f3480babdcbeb1840d1416c8437fcc0c9588d
rebecca-butler <rebutler@uwaterloo.ca>
2020-11-03 12:51:43 -0500

    Fixed syntax errors

--- src/counter.py
+++ src/counter.py
@@ -19,7 +19,6 @@ camera.framerate = 32
 camera.resolution = (640, 480)
 rawCapture = PiRGBArray(camera, size=(640, 480))
 time.sleep(0.1)
-now = date.now()
 
 ct = CentroidTracker(maxDisappeared=40, maxDistance=50)
 trackableObjects = {}
@@ -36,88 +35,87 @@ hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    image = frame.array
-    frame = imutils.resize(frame, width=500)
-    orig = image.copy()
+	image = frame.array
+	orig = image.copy()
 
 	if H is None or W is None:
-    	(H, W) = frame.shape[:2]
+		(H, W) = image.shape[:2]
 
-    # detect people in the image
-    (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),
-        padding=(8, 8), scale=1.05)
+	# detect people in the image
+	(rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),
+		padding=(8, 8), scale=1.05)
 
-    # draw initial bounding boxes
-    for (x, y, w, h) in rects:
-        cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
+	# draw initial bounding boxes
+	for (x, y, w, h) in rects:
+		cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-    # apply non-maxima suppression to bounding boxes (combines overlapping boxes into one)
-    rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
-    pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+	# apply non-maxima suppression to bounding boxes (combines overlapping boxes into one)
+	rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+	pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
 
-    # draw final bounding boxes
-    for (xA, yA, xB, yB) in pick:
-        cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
+	# draw final bounding boxes
+	for (xA, yA, xB, yB) in pick:
+		cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
 
-    # draw horizontal line across frame to determine crossing direction
-    cv2.line(frame, (0, H // 2), (W, H // 2), (0, 255, 255), 2)
+	# draw horizontal line across frame to determine crossing direction
+	cv2.line(image, (0, H // 2), (W, H // 2), (0, 255, 255), 2)
 
-    # use centroid tracker to associate old centroids with new centroids
-    objects = ct.update(rects)
+	# use centroid tracker to associate old centroids with new centroids
+	objects = ct.update(rects)
 
-    # loop over tracked objects
-    for (objectID, centroid) in objects.items():
-        to = trackableObjects.get(objectID, None)
+	# loop over tracked objects
+	for (objectID, centroid) in objects.items():
+		to = trackableObjects.get(objectID, None)
 
-        # if there is no existing trackable object, create one
-        if to is None:
-            to = TrackableObject(objectID, centroid)
+		# if there is no existing trackable object, create one
+		if to is None:
+			to = TrackableObject(objectID, centroid)
 
-        else:
-            # difference between y-coord of current centroid and mean of previous 
-            # centroids gives direction (down positive, up negative)
-            y = [c[1] for c in to.centroids]
-            direction = centroid[1] - np.mean(y)
-            to.centroids.append(centroid)
+		else:
+			# difference between y-coord of current centroid and mean of previous 
+			# centroids gives direction (down positive, up negative)
+			y = [c[1] for c in to.centroids]
+			direction = centroid[1] - np.mean(y)
+			to.centroids.append(centroid)
 
-            # check if object has been counted or not
-            if not to.counted:
-                # if direction is negative (object is moving up) and centroid is above center line, count object
-                if direction < 0 and centroid[1] < H // 2:
-                    numEntered += 1
-                    to.counted = True
+			# check if object has been counted or not
+			if not to.counted:
+				# if direction is negative (object is moving up) and centroid is above center line, count object
+				if direction < 0 and centroid[1] < H // 2:
+					numEntered += 1
+					to.counted = True
 
-                # if direction is positive (object is moving down) and centroid is below center line, count object
-                elif direction > 0 and centroid[1] > H // 2:
-                    numExited += 1
-                    to.counted = True
+				# if direction is positive (object is moving down) and centroid is below center line, count object
+				elif direction > 0 and centroid[1] > H // 2:
+					numExited += 1
+					to.counted = True
 
-        # store trackable object in dict
-        trackableObjects[objectID] = to
+		# store trackable object in dict
+		trackableObjects[objectID] = to
 
-        # draw ID of object and centroid of object on output frame
-        text = "ID {}".format(objectID)
-        cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        cv2.circle(frame, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
+		# draw ID of object and centroid of object on output frame
+		text = "ID {}".format(objectID)
+		cv2.putText(image, text, (centroid[0] - 10, centroid[1] - 10),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+		cv2.circle(image, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
 
-    # show and save output frame
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
-    cv2.imwrite('/home/pi/Projects/Videos/{}.jpg'.format(datetime.datetime.now()), frame)
+	# show and save output frame
+	cv2.imshow("Frame", image)
+	key = cv2.waitKey(1) & 0xFF
+	cv2.imwrite('/home/pi/Projects/Videos/{}.jpg'.format(datetime.datetime.now()), image)
 
 	# break if q pressed
-    if key == ord("q"):
-        break
+	if key == ord("q"):
+		break
 
-    # update global count
-    numInLibrary = numInLibrary + numEntered - numExited
-    print("Num in library: {}, num exited: {}, num entered: {}".format(numInLibrary, numExited, numEntered))
+	# update global count
+	numInLibrary = numInLibrary + numEntered - numExited
+	print("Num in library: {}, num exited: {}, num entered: {}".format(numInLibrary, numExited, numEntered))
 
-    # activate locking system if needed
-    if (numInLibrary > 45):
-        print("Maximum reached")
-        # activate lock
-    else:
-        # deactivate lock
-        print("Maximum has not been reached")
+	# activate locking system if needed
+	if (numInLibrary > 45):
+		print("Maximum reached")
+		# activate lock
+	else:
+		# deactivate lock
+ 		print("Maximum has not been reached")
